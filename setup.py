f7b027142e134fae21ffbf8dfa0a22ced11eb40d
Adam Davis <adavis444@users.noreply.github.com>
2023-06-09 19:44:31 -0400

    Build wheels for python 3.11

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup(
                  'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9',
                  'Programming Language :: Python :: 3.10'
+                 'Programming Language :: Python :: 3.11'
                  ],
     ext_modules=ext_modules,
     packages=packages,
